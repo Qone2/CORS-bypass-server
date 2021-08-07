a4ba269479906cd03cc2986a61c28d08265d076e
qone2 <whrbdnjs33@naver.com>
2021-08-08 01:46:23 +0900

    버그 가능성 수정
    img_file이 할당 전에 참조될 수 있는 문제 수정

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ def cors_bypass(img_path):
     custom_headers = {
         "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36"
     }
+    img_file = None
     try:
         img_file = requests.get(img_path.replace('¿', '?'), headers=custom_headers).content
     except requests.exceptions.MissingSchema:
